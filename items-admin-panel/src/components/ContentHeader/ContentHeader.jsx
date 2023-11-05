c8ddb37e523dbf15676e3ff729b885aabfc2b025
TodorZarkov <t.p.zarkov@gmail.com>
2023-11-05 10:16:18 +0200

    App component arranged and adapted

---
+++ items-admin-panel/src/components/ContentHeader/ContentHeader.jsx
@@ -0,0 +1,14 @@
+export const ContentHeader = () => {
+    return(
+        <section className="content-header">
+            <h1>
+              Dashboard
+              <small>Control panel</small>
+            </h1>
+            <ol className="breadcrumb">
+              <li><a href="#"><i className="fa fa-dashboard"></i> Home</a></li>
+              <li className="active">Dashboard</li>
+            </ol>
+          </section>
+    );
+}
