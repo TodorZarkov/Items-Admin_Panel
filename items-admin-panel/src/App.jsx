ad38e139b9b9b7d5cd3e2c4aff5a298f3fd51faa
TodorZarkov <t.p.zarkov@gmail.com>
2024-07-08 00:24:08 +0300

    Remove old skeleton

--- items-admin-panel/src/App.jsx
+++ items-admin-panel/src/App.jsx
@@ -1,29 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { Aside } from './components/Aside/Aside'
-import { MainContent } from './components/Content/MainContent'
-import { ContentHeader } from './components/ContentHeader/ContentHeader'
-import { Footer } from './components/Footer/Footer'
-import { Header } from './components/Header/Header'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-      // <div className="wrapper sidebar-collapse sidebar-open">
-      <div className="wrapper ">                         
-
-        <Header></Header>
-
-        <Aside></Aside>
-
-        <main className="content-wrapper">
-
-          <ContentHeader></ContentHeader>
-          <MainContent></MainContent>
-
-        </main>
-        
-        <Footer></Footer>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
