d680be67c1fc58af4b29c5b6f3cfc881fe243eae
TodorZarkov <t.p.zarkov@gmail.com>
2024-07-09 18:22:09 +0300

    Added regular user view html

--- items-admin-panel/src/App.jsx
+++ items-admin-panel/src/App.jsx
@@ -1,10 +1,70 @@
-import './App.css'
+
 
 function App() {
 
   return (
     <>
-      
+      <header>
+        <nav>
+          <ul role="list">
+            <li><a href="/"><img src=""></img></a></li>
+            <li><a href="/">Assigned</a></li>
+            <li><a href="/">Watched</a></li>
+            <li><a href="/">My Tickets</a></li>
+            <li><a href="/">Create</a></li>
+          </ul>
+          <ul role="list">
+            <li><a href="/">Units</a></li>
+            <li><a href="/">Categories</a></li>
+            <li><a href="/">Currencies</a></li>
+            <li><a href="/">Admins</a></li>
+          </ul>
+        </nav>
+        <section>
+          <ul role="list">
+            <li><a href="/"><p>Reported</p><p>00005235861</p></a></li>
+            <li><a href="/"><p>Opened</p><p>214</p></a></li>
+            <li><a href="/"><p>Solved</p><p>65235861</p></a></li>
+          </ul>
+        </section>
+      </header>
+      <main>
+        <section>
+          <form action="">
+            <button>T</button>
+            <button>S</button>
+            <button>U</button>
+            <button>T</button>
+            <input type="search" placeholder="SEARCH" />
+          </form>
+          <ul role="list">
+            <ul role="list">
+              <li>
+                <p>14:35</p>
+                <p>18</p>
+                <p>Jan</p>
+                <p>`24</p>
+              </li>
+              <li>
+                <p>3</p>
+              </li>
+              <li>
+                <p>358720</p>
+              </li>
+              <li>bug</li>
+              <li>
+                <a href="/">
+                  <img src="" alt="" />
+                </a>
+              </li>
+              <li>
+                <p>Doesn’t redirect after login. Goes to the main page every time. Doesn’t autofill properly the lo...</p>
+              </li>
+            </ul>
+          </ul>
+        </section>
+      </main>
+      <footer></footer>
     </>
   )
 }
