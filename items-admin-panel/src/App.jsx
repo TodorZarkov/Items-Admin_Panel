fe922af858e8e1914215911134da776806eace6a
TodorZarkov <t.p.zarkov@gmail.com>
2024-07-16 21:07:02 +0300

    Added dropdowns

--- items-admin-panel/src/App.jsx
+++ items-admin-panel/src/App.jsx
@@ -3,24 +3,53 @@
 function App() {
 
   return (
-    
+
     <>
-    {/* DETAILS */}
-    {/* CREATE */}
-    {/* DROPDOWN FOR MY TICKET TYPES */}
-    {/* DROPDOWN FOR THE AVATAR */}
+      {/* DETAILS */}
+      {/* CREATE */}
+      {/* DROPDOWN FOR MY TICKET TYPES */}
+      {/* DROPDOWN FOR THE AVATAR */}
       <header className="nav">
         <nav className="nav" >
           <ul role="list">
-            <p>
-              <a className="avatar" href="/">
-                <img src="/admin-logo.jpg" />
-              </a>
+            <p className="avatar dropdown">
+              <img className="dropbtn" src="/admin-logo.jpg" />
+              <div className="dropdown-content">
+                <a href="/">MenageProfile</a>
+                <a href="/">ChangePicture</a>
+              </div>
             </p>
             <li><a className="btn highlighted" href="/">Assigned</a></li>
-            <li><a className="btn important " href="/">Watched</a></li>
-            <li><a className="btn important " href="/">My Tickets</a></li>
-            <li><a className="btn important " href="/">Create</a></li>
+            <li className="dropdown">
+              <button className="btn important dropbtn" type="button">Watched</button>
+              <div className="dropdown-content">
+                <a href="/">Bugs</a>
+                <a href="/">Units</a>
+                <a href="/">Currencies</a>
+                <a href="/">Categories</a>
+                <a href="/">All</a>
+              </div>
+            </li>
+            <li className="dropdown">
+              <button className="btn important dropbtn" type="button">My Tickets</button>
+              <div className="dropdown-content">
+                <a href="/">Bugs</a>
+                <a href="/">Units</a>
+                <a href="/">Currencies</a>
+                <a href="/">Categories</a>
+                <a href="/">All</a>
+              </div>
+            </li>
+            <li className="dropdown">
+              <button className="btn important dropbtn" type="button">Create</button>
+              <div className="dropdown-content">
+                <a href="/">Bug</a>
+                <a href="/">Unit</a>
+                <a href="/">Currency</a>
+                <a href="/">Category</a>
+                <a href="/">Any</a>
+              </div>
+            </li>
           </ul>
           <ul role="list">
             <li><a className="btn secondary" href="/">Units</a></li>
