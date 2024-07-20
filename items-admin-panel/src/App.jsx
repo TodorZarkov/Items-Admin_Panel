e60c0376b802668cb7154a8d7d983738455dd350
TodorZarkov <t.p.zarkov@gmail.com>
2024-07-20 14:09:24 +0300

    Added routes

--- items-admin-panel/src/App.jsx
+++ items-admin-panel/src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom"
 import { CreateTicket } from "./components/CreateTicket"
 import { Header } from "./components/Header"
 import { Login } from "./components/Login"
@@ -8,20 +9,33 @@ import { Tickets } from "./components/Tickets"
 function App() {
 
   return (
-
     <>
-      {/* DETAILS */}
-      <Header />
-      <main >
-        <Tickets/>
-        {/* <Register/> */}
-        {/* <Login/> */}
-        {/* <CreateTicket/> */}
-      </main>
-     
+      <Routes>
+        {/* DETAILS */}
+        <Route path="/" element={
+          <>
+            <Header />
+            <Tickets />
+          </>
+        } />
+        <Route path="/login" element={
+          <>
+            <Header />
+            <Login />
+          </>
+        } />
+         <Route path="/register" element={
+          <>
+            <Header />
+            <Register />
+          </>
+        } />
+        <Route path="/create" element={<CreateTicket/> } />
+      </Routes>
+
       <footer></footer>
     </>
-  )
+  );
 }
 
 export default App
