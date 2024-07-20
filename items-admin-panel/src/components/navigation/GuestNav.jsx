51dd580e0d1d8868807f0a740a028ad0328f73f9
TodorZarkov <t.p.zarkov@gmail.com>
2024-07-20 11:20:33 +0300

    Second level integration

---
+++ items-admin-panel/src/components/navigation/GuestNav.jsx
@@ -0,0 +1,12 @@
+export function GuestNav() {
+    return (
+        <>
+            <li >
+                <a className="btn register" href="/">Register</a>
+            </li>
+            <li >
+                <a className="btn login" href="/">Login</a>
+            </li>
+        </>
+    );
+}
---
+++ items-admin-panel/src/components/navigation/SecondaryNav.jsx
@@ -0,0 +1,10 @@
+export function SecondaryNav() {
+    return (
+        <ul role="list">
+            <li><a className="btn secondary" href="/">Units</a></li>
+            <li><a className="btn secondary" href="/">Categories</a></li>
+            <li><a className="btn secondary" href="/">Currencies</a></li>
+            <li><a className="btn secondary" href="/">Admins</a></li>
+        </ul>
+    );
+}
