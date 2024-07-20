2a938e29bb401f313751e481d0749bafb3e5f1b1
TodorZarkov <t.p.zarkov@gmail.com>
2024-07-20 14:32:48 +0300

    added guest nav conditional rendering

--- items-admin-panel/src/components/navigation/GuestNav.jsx
+++ items-admin-panel/src/components/navigation/GuestNav.jsx
@@ -1,12 +1,32 @@
 export function GuestNav() {
-    return (
-        <>
+    const path = location.pathname;
+
+    if (path == '/login') {
+        return (
             <li >
                 <a className="btn register" href="/">Register</a>
             </li>
+        );
+
+    } else if (path == '/register') {
+        return (
             <li >
                 <a className="btn login" href="/">Login</a>
             </li>
-        </>
-    );
+        );
+
+    } else {
+        return (
+            <>
+                <li >
+                    <a className="btn register" href="/">Register</a>
+                </li>
+                <li >
+                    <a className="btn login" href="/">Login</a>
+                </li>
+            </>
+        );
+
+
+    }
 }
