ae22046cc5be49883788516e1ff78f0e1dda37b4
TodorZarkov <t.p.zarkov@gmail.com>
2024-07-21 13:40:24 +0300

    added skeleton of useLocalStorage

---
+++ items-admin-panel/src/hooks/useLocalStorage.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export function useLocalStorage(key, initialValue) {
+
+
+
+    function setLocalState(newState) {
+
+    };
+
+    return [state, setLocalState];
+}
