5e3ac0f68e076bd9445866753fced656d252cef0
TodorZarkov <t.p.zarkov@gmail.com>
2024-07-21 10:31:03 +0300

    Added server api dependent on session storage and bearer token

---
+++ items-admin-panel/src/services/serverApi.js
@@ -0,0 +1,49 @@
+const domain = "https://localhost:7229";
+//depends on:
+//-user stringified  object in session storage;
+//-user.token (the Base64 JWT without the "Bearer" word)
+
+async function request(method, uri, data) {
+    const user = JSON.parse(localStorage.getItem("user"));
+    let requestObj = {
+        method,
+        headers: { "content-type": "application/json" }
+    }
+
+
+    if (method === "post" || method === "put" || method === "patch") {
+        requestObj.body = JSON.stringify(data);
+    }
+    if (user) {
+        requestObj.headers["Authorization"] = `Bearer ${user.token}`;
+    }
+    console.log('from server api before fetch, user: ', user);
+    try {
+        let response = await fetch(domain + uri, requestObj);
+
+        if (response.status === 204) {
+            return response;
+        }
+
+        if (!response.ok) {
+            if(response.status === 403) {
+                localStorage.removeItem("user");
+            }
+            const err = await response.json();
+            throw new Error(err.message);
+        }
+
+        let result = await response.json();
+        return result;
+
+    } catch (error) {
+        //alert(error.message);
+        throw error;
+    }
+
+}
+
+export let get = request.bind(null, "get");
+export let post = request.bind(null, "post");
+export let put = request.bind(null, "put");
+export let del = request.bind(null, "delete");
