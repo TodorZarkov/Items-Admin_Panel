2b85749fa769eaf1d5344ac7e587ebf0c6c71b9e
TodorZarkov <t.p.zarkov@gmail.com>
2024-08-03 18:07:29 +0300

    added loader spinner to  afflicted button

--- items-admin-panel/src/components/TicketDetails/AfflictedButton/AfflictedButton.jsx
+++ items-admin-panel/src/components/TicketDetails/AfflictedButton/AfflictedButton.jsx
@@ -4,31 +4,44 @@ import { useServiceWithAuth } from "../../../hooks/useServiceWithAuth";
 import { ticketServiceFactory } from "../../../services/ticketService";
 
 export function AfflictedButton({
-    iHaveSameProblem, 
-    id}) {
+    iHaveSameProblem,
+    id }) {
     const ticketService = useServiceWithAuth(ticketServiceFactory);
 
     const [toggle, setToggle] = useState(iHaveSameProblem);
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         setToggle(state => state = iHaveSameProblem);
     }, [iHaveSameProblem]);
 
     function onToggle() {
-        try {
-            ticketService.update(id, {"toggleSameProblem": true})
-            .then(()=>setToggle(state => !state));
-        } catch (err) {
-            setError(state => state = true);
-        }
-       
+        setLoading(state => state = true)
+        ticketService.update(id, { "toggleSameProblem": true })
+            .then(() => {
+                setToggle(state => !state);
+                setLoading(state => state = false)});
     }
-    return(
-        <button 
+
+    let buttonContent = "Loading";
+    let frameClass = `${s.frame}`;
+    if (toggle !== undefined && !loading) {
+        if (toggle === true) {
+            buttonContent = "I don't have same problem";
+        } else {
+            buttonContent = "I Have Same Problem";
+        }
+        frameClass = "";
+    }
+    return (
+        <div className={frameClass}>
+            <button
                 type="button"
                 className={`${s.btn} ${toggle ? "" : s.notAfflicted}`}
                 onClick={onToggle}
-        >
-            {(toggle ?"I don't have same problem":"I Have Same Problem")}
-        </button>
+                disabled={toggle === undefined || loading ? true : false}
+            >{buttonContent}</button>
+        </div>
+
     )
 }
