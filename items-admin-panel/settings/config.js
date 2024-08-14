57ec51351f098d2206f6f29fa46925b3a71fe2c7
TodorZarkov <t.p.zarkov@gmail.com>
2024-08-14 22:45:58 +0300

    changed the server config file

--- items-admin-panel/settings/config.js
+++ items-admin-panel/settings/config.js
@@ -1,2 +1,2 @@
-export const apiHost = 'https://localhost:7229/api';
-export const host = 'https://localhost:7121'
+export const apiHost = 'https://todor.sytes.net/api';//'https://localhost:7229/api';
+export const host = 'https://todor.sytes.net';//'https://localhost:7121'
