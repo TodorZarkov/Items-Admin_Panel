68df9bfe48ab2c6f5249ced0a691e773ef8d4090
TodorZarkov <t.p.zarkov@gmail.com>
2024-12-13 18:19:21 +0200

    Link properly the serving site

--- items-admin-panel/src/components/Welcome.jsx
+++ items-admin-panel/src/components/Welcome.jsx
@@ -1,9 +1,9 @@
-
+import {host} from "../../settings/config";
 export function Welcome() {
     return (
         <>
             <h2>Welcome to Items Admin Panel</h2>
-            <a href="https://todor.sytes.net">Go to Items instead</a>
+            <a href={host}>Go to Items instead</a>
             <p>TODO: MAKE IT PRETTIER</p>
         </>
     );
